b051a0425346f407385a54598175fd9d17214ade
unknown <sixstrings22@outlook.com>
2023-11-13 21:15:34 -0600

    bulma hamburger working as intended

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -138,5 +138,23 @@ function displayRecommenedCocktails(cocktail){ // takes in a cocktail
 
 }
 
+// Adds or removes is-active class based on current status to allow burger to work on mobile as intended
+function burgerFunction() {
+  var burgerBtn = document.getElementById("burgerBtn");
+  var navMenu = document.getElementById("navMenu");
+
+  if (!burgerBtn.classList.contains('is-active')) {
+    burgerBtn.classList.add("is-active");
+  } else {
+    burgerBtn.classList.remove("is-active");
+  }
+
+  if (!navMenu.classList.contains('is-active')) {
+    navMenu.classList.add("is-active");
+  } else {
+    navMenu.classList.remove("is-active");
+  }
+}
+
 displayRecommenedMeal(recommendedMeal);
 displayRecommenedCocktails(recommendedCocktail);
